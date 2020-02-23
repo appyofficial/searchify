72c484aa6b9ef97a0f4c9a4a27f5b705ebcfaf2c
Appy Sharma <appy70890@gmail.com>
2020-02-23 12:54:18 +0100

    v 0.1

--- js/index.js
+++ js/index.js
@@ -22,7 +22,24 @@ function searchResults() {
                 html = '<h2>Artist Not Found!</h2>';
             } else {
                 artists.forEach(artist => {
-                    html = `<p>${artist.strArtist}</p>`;
+                    let artistBio = artist.strBiographyEN.split('.')[0];
+                    html += `<section class="artist-card">
+                    <div class="artist-img"><img src=${artist.strArtistThumb} alt="coldplay"></div>
+                    <div class="artist-bio">
+                        <div class="artist-bio-header">
+                        <h3>${artist.strArtist}</h3>
+                        <p>${artist.strGenre}</p>
+                        <div class="artist-links">
+                            <a href=${artist.strWebsite} target="_blank">Website</a>
+                            <a href=${artist.strTwitter} target="_blank">Twitter</a>
+                            <a href=${artist.strFacebook} target="_blank">Facebook</a>
+                        </div>
+                    </div>
+                        <p class="artist-bio-sd">${artistBio}.</p>
+                        <a href="#" class="artist-bio-view-more">View More</a>
+                    </div>
+                </section>`;
+                console.log(artist.strWebsite);
                 });
             }
             searchContainer.innerHTML = html;
